664419c70075d9c49aca67756c010cc719797cf8
Muhammad Abdul Rehman <abdulrehmanmuhammad424@gmail.com>
2025-08-18 02:17:15 +0500

    login with signup page

---
+++ src/components/Login.jsx
@@ -0,0 +1,148 @@
+import React, { useState } from "react";
+import TCCImage from "../assets/Images/TCC_bg.jpg";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import logo from "../assets/SVG/logo.svg";
+import apple from "../assets/SVG/apple.svg";
+import shopify from "../assets/SVG/shopify.svg";
+import google from "../assets/SVG/google.svg";
+import { Link } from "react-router-dom";
+
+
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <div
+      className="min-h-screen flex bg-contain relative"
+      style={{ backgroundImage: `url(${TCCImage})` }}
+    >
+      <div className="w-1/2 relative flex flex-col justify-between text-white p-5">
+        <div className="font-bold text-2xl">
+          <img src={logo} alt="" />
+        </div>
+        <div className="bg-gradient-to-b from-[#F77F00]/70 to-[#666666]/70 p-8 rounded-lg">
+          <h2 className="text-7xl  font-semibold ">
+            <span className="text-orange-400">Grow</span> with every traveler
+          </h2>
+          <p className="mt-3 text-lg text-gray-200">
+            Reach high-value guests where they stay. List your best pieces and
+            get paid without adding headcount
+          </p>
+        </div>
+      </div>
+
+      <div className="w-1/2 bg-white flex items-center justify-center">
+        <div className="w-full max-w-[480px]  ">
+          <h2 className="text-4xl font-bold font-Roboto Serif text-[#232323] mb-2 flex items-center justify-center ">
+            Welcome back!
+          </h2>
+          <p className="text-gray-500 text-sm mb-10 flex items-center justify-center pt-3">
+            Let’s get back to your business!
+          </p>
+
+          <form className="space-y-3">
+            <div className="flex flex-col gap-y-6">
+              <div className="relative">
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  className="block p-4 pt-4 w-full text-sm text-[#202020] bg-transparent rounded-xl border-1 border-[#D9D9D9] focus:outline-none focus:ring-0 focus:border-[#D9D9D9] peer"
+                  placeholder=" "
+                />
+                <label
+                  for="email"
+                  className="absolute text-base ms-4 text-gray-500  duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#202020]  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto   start-1"
+                >
+                  Email
+                </label>
+              </div>
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  className="block p-4 pt-4 w-full text-sm text-[#202020] bg-transparent rounded-xl border-1 border-[#D9D9D9]  focus:outline-none focus:ring-0 focus:border-[#D9D9D9] peer"
+                  placeholder=" "
+                />
+                <label
+                  for="password"
+                  className="ms-3 absolute text-base text-gray-500  duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#202020]  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                >
+                  Password
+                </label>
+
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                >
+                  {showPassword ? <FiEyeOff size={20} /> : <FiEye size={20} />}
+                </button>
+              </div>
+            </div>
+            <div className="flex items-center justify-between text-sm mb-6">
+              <label className="flex items-center space-x-2">
+               
+                <input
+                  type="checkbox"
+                  className="w-[18px] h-[18px] rounded-md appearance-none border border-gray-300 
+               checked:bg-orange-500 checked:border-orange-500 
+               relative 
+               checked:after:content-[''] 
+               checked:after:absolute checked:after:inset-0 checked:after:mx-auto checked:after:mt-[1px]
+               checked:after:w-[6px] checked:after:h-[10px] 
+               checked:after:border-r-2 checked:after:border-b-2 
+               checked:after:border-white checked:after:rotate-45"
+                  checked
+                />
+                <span>Remember me</span>
+              </label>
+              <a href="/" className="text-[#F77F00] hover:underline font-semibold">
+                Forgot password?
+              </a>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-[#F77F00] text-white text-base py-4 rounded-lg transition "
+            >
+              Sign in
+            </button>
+          </form>
+          <div className="flex items-center my-4 py-0.5 px-1">
+            <div className="flex-grow border-t border-gray-300"></div>
+            <span className="mx-2 text-gray-500 text-xs ">or sign in with</span>
+            <div className="flex-grow border-t border-gray-300"></div>
+          </div>
+
+          <div className="space-y-3">
+            <button className="w-full border border-[#D9D9D9] py-4 rounded-lg flex items-center justify-center gap-3 hover:bg-gray-50">
+              <img src={google} alt="" />
+              <span className="text-base">Sign in with Google</span>
+            </button>
+
+            <div className="flex space-x-4">
+              <button className="w-1/2 border border-[#D9D9D9] py-4 rounded-lg flex items-center justify-center gap-3 hover:bg-gray-50">
+                <img src={apple} alt="" />
+                <span className="text-base">Sign in with Apple</span>
+              </button>
+              <button className="w-1/2 border border-[#D9D9D9] py-4 rounded-lg flex items-center justify-center gap-3 hover:bg-gray-50">
+                <img src={shopify} alt="" />
+                <span>Sign in with Shopify</span>
+              </button>
+            </div>
+          </div>
+
+          <p className="text-center text-base text-gray-600 mt-4">
+            Don’t have an account?{" "}
+            <Link to="/signup" className="text-[#F77F00] hover:underline">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Signup.jsx
@@ -0,0 +1,160 @@
+import React, { useState } from "react";
+import TCCImage from "../assets/Images/TCC_bg.jpg";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import logo from "../assets/SVG/logo.svg";
+import { Link } from "react-router-dom";
+
+
+const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  return (
+    <div
+      className="min-h-screen flex bg-contain relative"
+      style={{ backgroundImage: `url(${TCCImage})` }}
+    >
+      <div className="w-1/2 relative flex flex-col justify-between text-white p-5">
+        <div className="font-bold text-2xl">
+          <img src={logo} alt="" />
+        </div>
+        <div className="bg-gradient-to-b from-[#F77F00]/70 to-[#666666]/70 p-8 rounded-lg">
+          <h2 className="text-7xl  font-semibold ">
+            <span className="text-orange-400">Grow</span> with every traveler
+          </h2>
+          <p className="mt-3 text-lg text-gray-200">
+            Reach high-value guests where they stay. List your best pieces and
+            get paid without adding headcount
+          </p>
+        </div>
+      </div>
+
+      <div className="w-1/2 bg-white flex items-center justify-center">
+        <div className="w-full max-w-[480px]  ">
+          <h2 className="text-4xl font-bold font-Roboto Serif text-[#232323] mb-2 flex items-center justify-center ">
+            Sign Up
+          </h2>
+          <p className="text-gray-500 text-sm mb-10 flex items-center justify-center pt-3">
+            Welcome to start your business!
+          </p>
+
+          <form className="space-y-3">
+            <div className="flex flex-col gap-y-6">
+              <div className="relative">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="block p-4 pt-4 w-full text-sm text-[#202020] bg-transparent rounded-xl border-1 border-[#D9D9D9] focus:outline-none focus:ring-0 focus:border-[#D9D9D9] peer"
+                  placeholder=" "
+                />
+                <label
+                  for="name"
+                  className="absolute text-base ms-4 text-gray-500  duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#202020]  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto   start-1"
+                >
+                  Name
+                </label>
+              </div>
+              <div className="relative">
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  className="block p-4 pt-4 w-full text-sm text-[#202020] bg-transparent rounded-xl border-1 border-[#D9D9D9] focus:outline-none focus:ring-0 focus:border-[#D9D9D9] peer"
+                  placeholder=" "
+                />
+                <label
+                  for="email"
+                  className="absolute text-base ms-4 text-gray-500  duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#202020]  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto   start-1"
+                >
+                  Email
+                </label>
+              </div>
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  className="block p-4 pt-4 w-full text-sm text-[#202020] bg-transparent rounded-xl border-1 border-[#D9D9D9]  focus:outline-none focus:ring-0 focus:border-[#D9D9D9] peer"
+                  placeholder=" "
+                />
+                <label
+                  for="password"
+                  className="ms-3 absolute text-base text-gray-500  duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#202020]  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                >
+                  Password
+                </label>
+
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                >
+                  {showPassword ? <FiEyeOff size={20} /> : <FiEye size={20} />}
+                </button>
+              </div>
+              <div className="relative">
+                <input
+                  type={showConfirmPassword ? "text" : "password"}
+                  id="confirm password"
+                  name="confirme_password"
+                  className="block p-4 pt-4 w-full text-sm text-[#202020] bg-transparent rounded-xl border-1 border-[#D9D9D9]  focus:outline-none focus:ring-0 focus:border-[#D9D9D9] peer"
+                  placeholder=" "
+                />
+                <label
+                  for="confirm password"
+                  className="ms-3 absolute text-base text-gray-500  duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#202020]  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+                >
+                  Confirm Password
+                </label>
+                <button
+                  type="button"
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 courser"
+                >
+                  {showConfirmPassword ? (
+                    <FiEyeOff size={20} />
+                  ) : (
+                    <FiEye size={20} />
+                  )}
+                </button>
+              </div>
+            </div>
+            <div className="flex items-center justify-between text-sm mb-6">
+              <label className="flex items-center space-x-2 cursor-pointer">
+                <input
+                  type="checkbox"
+                  className="w-[18px] h-[18px] rounded-md appearance-none border border-gray-300 
+               checked:bg-orange-500 checked:border-orange-500 
+               relative 
+               checked:after:content-[''] 
+               checked:after:absolute checked:after:inset-0 checked:after:mx-auto checked:after:mt-[2px]
+               checked:after:w-[6px] checked:after:h-[10px] 
+               checked:after:border-r-2 checked:after:border-b-2 
+               checked:after:border-white checked:after:rotate-45"
+                />
+                <span className="text-gray-700">I agree with the <a href="#" class="text-[#F77F00] hover:underline">terms and conditions</a>.</span>
+              </label>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full mt-4 bg-[#F77F00] text-white text-base py-4 rounded-lg transition "
+            >
+              Sign up
+            </button>
+          </form>
+
+          <p className="text-center text-base text-gray-600 mt-5">
+            If you have an account?{" "}
+            <Link to="/login" className="text-[#F77F00] hover:underline">
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
